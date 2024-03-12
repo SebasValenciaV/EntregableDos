42f3e4aa32a234289a7157c533a90024fb96db1d
SebasValenciaV <sebas1234989252@gmail.com>
2024-03-12 12:53:53 -0500

    new user

--- tarea.js
+++ tarea.js
@@ -227,7 +227,7 @@ const crearInstanciaPersona = (nombre, apellido, edad, dir)=>{
     //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
     //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
     //Devolver la nueva persona creada
-    let persona2 = new Persona(nombre = "Sebas", apellido="Valencia",edad=22,dir="sebas333");
+    let persona2 = new Persona(nombre = "Sebas", apellido="Valencia",edad=22,dir="333");
     return persona2;
     
 }
